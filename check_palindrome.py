7db8eb012e408a77c80fc2f0e283f26e38beb35d
Omar <bomarm567@gmail.com>
2024-02-08 07:31:35 -0600

    Refacted palindrome check to use a function

--- check_palindrome.py
+++ check_palindrome.py
@@ -1,6 +1,8 @@
-text = "did"
+def check_palindrome(text):
+   if text == text[::-1]:
+      print(f"{text} is a palindrome")
+   else:
+      print(f"{text} is not a palindrome")
 
-if text == text[::-1]:
-   print(f"{text} is a palindrome")
-else:
-   print(f"{text} is not a palindrome")
+text = "did"
+check_palindrome(text)
